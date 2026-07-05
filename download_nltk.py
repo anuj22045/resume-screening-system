1f8f5d9bd3a3fb050ed5cb8ffd430cce2ba567ba
Anuj Kumar <144164000+anuj22045@users.noreply.github.com>
2026-07-05 11:44:39 +0530

    update download_nltk

--- download_nltk.py
+++ download_nltk.py
@@ -1,7 +1,13 @@
 import nltk
 
-nltk.download('punkt_tab')
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('omw-1.4')
+resources = [
+    "punkt",
+    "punkt_tab",
+    "stopwords",
+    "wordnet",
+    "omw-1.4"
+]
+
+for resource in resources:
+    print(f"Downloading {resource}...")
+    nltk.download(resource)
